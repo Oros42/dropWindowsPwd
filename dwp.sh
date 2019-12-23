c7c3de9ffa92982ff2a5d53168b43869983a664f
Oros42 <oros@ecirtam.net>
2019-12-23 17:54:34 +0100

    init

---
+++ dwp.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# Author : Oros
+# 2019-12-23
+# https://github.com/Oros42/dropWindowsPwd
+#
+# This script remove all Windows accound's passord it found.
+#
+# You need :
+# sudo apt install mount ntfs-3g chntpw coreutils gawk
+
+tmpMount="/media/tmpMount"
+mkdir -p $tmpMount
+for p in ls /dev/disk/by-partuuid/*; do
+	# for all partitions
+	if [ "$(file -sL $p |grep NTFS)" != "" ]; then
+		# Only NTFS patitions
+		ntfsfix $p
+		echo "mount -t ntfs-3g $p $tmpMount"
+		mount -t ntfs-3g $p $tmpMount
+		if [ -f $tmpMount/Windows/System32/config/SAM ]; then
+			cp $tmpMount/Windows/System32/config/SAM $tmpMount/Windows/System32/config/SAM.back_$(date +%s)
+		fi
+
+		# List of accounts
+		chntpw -l $tmpMount/Windows/System32/config/SAM
+
+		for rid in $(chntpw -l $tmpMount/Windows/System32/config/SAM|tail -n +8|awk -F" " '{print $2}' ); do
+			# for all user
+			echo "RID :$rid"
+			# 2 : Unlock and enable user account
+			# 1 : Clear (blank) user password
+			echo -e "2\n1\nq\ny\n" | chntpw -u 0x$rid $tmpMount/Windows/System32/config/SAM
+		done
+
+		# List of accounts after changes
+		chntpw -l $tmpMount/Windows/System32/config/SAM
+
+		umount $tmpMount
+	fi
+done
